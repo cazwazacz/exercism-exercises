f079aaf3894187769c488d095757a6d6c6165862
Allan Wazacz <cazwazacz@gmail.com>
2019-01-13 11:33:45 +0000

    Add comment

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,7 +1,9 @@
+// Package twofer contains a function which returns a sharing string.
 package twofer
 
 import "fmt"
 
+// ShareWith takes a name and returns a "One for you, one for me." type string.
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
