0ecb9cb4c92867ac1b66fbe6a469b94158dd44ca
Allan Wazacz <cazwazacz@gmail.com>
2019-01-13 18:23:50 +0000

    Hamming exercise

---
+++ go/hamming/hamming.go
@@ -0,0 +1,26 @@
+package hamming
+
+import (
+	"errors"
+)
+
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("strands must be of the same length")
+	}
+
+	return checkDifference(a, b), nil
+}
+
+func checkDifference(a, b string) int {
+	difference := 0
+
+	for i := range a {
+		if a[i] != b[i] {
+			difference++
+		}
+		// fmt.Printf("The characrer %c is at position %d", char, pos)
+	}
+
+	return difference
+}
