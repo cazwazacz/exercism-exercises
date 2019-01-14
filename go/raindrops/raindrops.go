e7402bb86b9274b54fe124dbe232b38d08479d4a
Allan Wazacz <cazwazacz@gmail.com>
2019-01-14 23:51:23 +0000

    Refactor

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -5,7 +5,18 @@ import (
 	"strings"
 )
 
-// Convert take an integer and returns a string depending on which factors the numbers have
+type factor struct {
+	number int
+	string string
+}
+
+var factors = []factor{
+	factor{3, "Pling"},
+	factor{5, "Plang"},
+	factor{7, "Plong"},
+}
+
+// Convert takes an integer and returns a string depending on which factors the numbers have
 // if the number has 3 as a factor, output "Pling"
 // if the number has 5 as a factor, output "Plang"
 // if the number has 7 as a factor, output "Plong"
@@ -13,9 +24,9 @@ import (
 func Convert(number int) string {
 	var stringBuilder strings.Builder
 
-	stringBuilder.WriteString(checkFactor(number, 3, "Pling"))
-	stringBuilder.WriteString(checkFactor(number, 5, "Plang"))
-	stringBuilder.WriteString(checkFactor(number, 7, "Plong"))
+	for _, factor := range factors {
+		stringBuilder.WriteString(checkFactor(number, factor.number, factor.string))
+	}
 
 	if stringBuilder.String() == "" {
 		return strconv.Itoa(number)
