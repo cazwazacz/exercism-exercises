93c6ae82e28074e9eeec491efe446466a1b41c75
Allan Wazacz <cazwazacz@gmail.com>
2019-01-15 22:19:19 +0000

    Scrabble scores

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,6 +1,31 @@
 package scrabble
 
-func Score() {}
+import "strings"
+
+// Score takes a string and returns its scrabble score
+func Score(word string) int {
+	score := 0
+
+	for _, character := range word {
+		for points, letters := range lettersToPoints {
+			if letterInSlice(character, letters) {
+				score += points
+			}
+		}
+	}
+
+	return score
+}
+
+func letterInSlice(character rune, slice []string) bool {
+	for i := range slice {
+		if strings.ToUpper(string(character)) == slice[i] {
+			return true
+		}
+	}
+
+	return false
+}
 
 var lettersToPoints = map[int][]string{
 	1:  {"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"},
