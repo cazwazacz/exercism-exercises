398a70766cb8afb33b62c4b2775daa1f0cf4371c
Allan Wazacz <cazwazacz@gmail.com>
2019-01-19 17:54:09 +0000

    Change data structure for efficiency

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,38 +1,20 @@
 package scrabble
 
-import "strings"
+import (
+	"unicode"
+)
+
+var letterToPoints = map[rune]int{
+	'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10,
+}
 
 // Score takes a string and returns its scrabble score
 func Score(word string) int {
 	score := 0
 
-	for _, character := range word {
-		for points, letters := range lettersToPoints {
-			if letterInSlice(character, letters) {
-				score += points
-			}
-		}
+	for _, letter := range word {
+		score += letterToPoints[unicode.ToUpper(letter)]
 	}
 
 	return score
-}
-
-func letterInSlice(character rune, slice []string) bool {
-	for i := range slice {
-		if strings.ToUpper(string(character)) == slice[i] {
-			return true
-		}
-	}
-
-	return false
-}
-
-var lettersToPoints = map[int][]string{
-	1:  {"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"},
-	2:  {"D", "G"},
-	3:  {"B", "C", "M", "P"},
-	4:  {"F", "H", "V", "W", "Y"},
-	5:  {"K"},
-	8:  {"J", "X"},
-	10: {"Q", "Z"},
 }
